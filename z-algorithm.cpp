2f6f9c1b0285d974c02c9d8083a1a01d7e9ce93f
mjzyaad <34031435+mjzyaad@users.noreply.github.com>
2019-04-29 14:15:39 +0200

    More information added as comments

--- z-algorithm.cpp
+++ z-algorithm.cpp
@@ -1,3 +1,12 @@
+// Z Algorithm
+//
+// O(N) string comparison algorithm
+// For a string s of length n, the function returns
+// an aeeay z, where z[i] is the length of the
+// longest substring, starting at i and which is also
+// the prefix of s.
+//
+// Reference: https://cp-algorithms.com/string/z-function.html
 vector<int> z_function(string s) {
   int n = s.length();
   vector<int> z(n, 0);
